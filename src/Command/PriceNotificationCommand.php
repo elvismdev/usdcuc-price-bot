992c893f4febe0062d70b03813fe9ce277e65c9a
Elvis Morales <elvismdev@gmail.com>
2020-01-26 17:12:19 -0800

    Get each Ad URI.

--- src/Command/PriceNotificationCommand.php
+++ src/Command/PriceNotificationCommand.php
@@ -45,6 +45,77 @@ class PriceNotificationCommand extends Command
         // Get banned words from settings.
         $bannedWords = $this->getParameter('banned_words');
         $bannedWords = UtilityBox::addExclPrefix($bannedWords);
+
+        // Create string with banned words.
+        $bannedWordsStr = implode(' ', $bannedWords);
+
+        // Create the full power keyword search text.
+        $searchQuery = '"' . $this->getParameter('search_text') . '" ' . $bannedWordsStr;
+
+        // Make an HTTP GET request to https://www.revolico.com/compra-venta/divisas/search.html?q=...&min_price=...&max_price=...
+        $response = $client->request('GET', $this->getParameter('search_page_url'), [
+            // Set request headers.
+            'headers' => [
+                'User-Agent' => $this->getParameter('user_agent')
+            ],
+
+            // Set search parameters. These values are automatically encoded before including them in the URL
+            'query' => [
+                'q'         => $searchQuery,
+                'min_price' => $this->getParameter('min_price'),
+                'max_price' => $this->getParameter('max_price'),
+            ],
+        ]);
+
+        // Get the status code.
+        $statusCode = $response->getStatusCode();
+
+        // Check status of request.
+        if (200 !== $statusCode) {
+            $io->error(sprintf('Command failed with error: %s', $statusCode));
+        } else {
+
+            // Get the HTML contents of the page requested.
+            $content = $response->getContent();
+
+            // Send HTML to crawler.
+            $crawler = new Crawler($content);
+
+            // Obtain all the ads <li> rows.
+            $adRowElements = $crawler
+            ->filter('li[data-cy="adRow"]')
+            // Exclude Ads with banned words in the title.
+            ->reduce(function (Crawler $node, $i) use ($bannedWords) {
+                // Get the adTitle element.
+                $adTitleElement = $node->filter('span[data-cy="adTitle"]');
+
+                // Check if Ad title contains banned words.
+                if ($this->striposa($adTitleElement->html(), $bannedWords) === false) {
+                    // Include this Ad, it seems to NOT have banned word in his title.
+                    return true;
+                } else {
+                    // Do not include this Ad, it seems it has a banned word in his title.
+                    return false;
+                }
+            })
+            ;
+
+            // Loop each Ad, stores in the DB if doesn't exists yet and sends a notification with a list of new Ads found.
+            foreach ($adRowElements as $domElement) {
+                // Continue to next element if Ad price is set in CUP.
+                if (strpos($domElement->firstChild->firstChild->nodeValue, 'CUP') !== false) {
+                    continue;
+                }
+
+                // $io->note(sprintf('Ad: %s', print_r($domElement->firstChild->firstChild->nodeValue, true)));
+
+                // Get Ad URI.
+                $adUri = $domElement->firstChild->getAttribute('href');
+            }
+
+            $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        }
+
         
         // $arg1 = $input->getArgument('arg1');
 
@@ -56,11 +127,27 @@ class PriceNotificationCommand extends Command
         //     // ...
         // }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        
 
         return 0;
     }
-    
+
+
+    /**
+     * Custom stripos() function to find multiple needles in one haystack.
+     * @param string $haystack
+     * @param array $needle
+     * @param bool $offset
+     * @return bool
+     */
+    private function striposa($haystack, $needle, $offset=0) {
+        if(!is_array($needle)) $needle = array($needle);
+        foreach($needle as $query) {
+            if(stripos($haystack, $query, $offset) !== false) return true;
+        }
+        return false;
+    }
+
 
     /**                                 
      * Get parameter from ParameterBag                                           
